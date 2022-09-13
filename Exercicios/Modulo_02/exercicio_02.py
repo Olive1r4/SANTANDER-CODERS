2c516a21302aea60ea8b613d6358fddca3e4af20
Fernando Oliveira <fernandooliveira@live.com>
2022-09-13 20:05:36 -0300

    Exercício 02

---
+++ Exercicios/Modulo_02/exercicio_02.py
@@ -0,0 +1,7 @@
+# Faça um programa que peça para o usuário digitar uma palavra e imprima cada letra em uma linha.
+
+def main():
+    palavra = input('Digite uma palavra: ')
+    for letra in palavra:
+        print(letra)
+main()
